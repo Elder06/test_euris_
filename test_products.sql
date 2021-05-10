980f42d1829dc984be7a9013cd08bf548843df4a
Elder06 <82147841+Elder06@users.noreply.github.com>
2021-05-10 17:55:59 +0200

    Add files via upload

---
+++ test_products.sql
@@ -0,0 +1,28 @@
+create table products
+(
+    serial_number varchar(50) not null
+        primary key,
+    name          varchar(90) null,
+    price         varchar(12) null
+);
+
+INSERT INTO test.products (serial_number, name, price) VALUES ('0519948289', 'Ensulizole', '0,7,8');
+INSERT INTO test.products (serial_number, name, price) VALUES ('0567155013', 'Acetaminophen, Aspirin, Caffeine', '8,4,5');
+INSERT INTO test.products (serial_number, name, price) VALUES ('1499303165', 'Carvedilol', '7,10,8');
+INSERT INTO test.products (serial_number, name, price) VALUES ('1575337517', 'Acetaminophen', '2,4,2');
+INSERT INTO test.products (serial_number, name, price) VALUES ('2272260632', 'Naproxen', '8,4,5');
+INSERT INTO test.products (serial_number, name, price) VALUES ('3570580482', 'oxytocin', '10,7,11');
+INSERT INTO test.products (serial_number, name, price) VALUES ('4111046037', 'Octinoxate', '10,7,11');
+INSERT INTO test.products (serial_number, name, price) VALUES ('4493438525', 'Levofloxacin', '10,7,11');
+INSERT INTO test.products (serial_number, name, price) VALUES ('5307369654', 'levofloxacin', '2,4,2');
+INSERT INTO test.products (serial_number, name, price) VALUES ('5407243431', 'BELATACEPT', '0,0,9');
+INSERT INTO test.products (serial_number, name, price) VALUES ('6029740547', 'Alcohol', '7,8,10');
+INSERT INTO test.products (serial_number, name, price) VALUES ('6064650532', 'Acetaminophen, Dextromethorphan Hydrobromide, Phenylephrine Hydrochloride', '8,4,5');
+INSERT INTO test.products (serial_number, name, price) VALUES ('6954637351', 'argipressin', '5,4,5');
+INSERT INTO test.products (serial_number, name, price) VALUES ('7158289583', 'Doxycycline Hyclate', '7,4,5');
+INSERT INTO test.products (serial_number, name, price) VALUES ('7297131903', 'Avobenzone, Homosalate, Octisalate, and Octocrylene', '8,4,5');
+INSERT INTO test.products (serial_number, name, price) VALUES ('7298477455', 'Benazepril Hydrochloride and Hydrochlorothiazide', '8,3,10');
+INSERT INTO test.products (serial_number, name, price) VALUES ('8367242572', 'LOVASTATIN', '9,6,8');
+INSERT INTO test.products (serial_number, name, price) VALUES ('8958967129', 'ketamine hydrochloride', '2,4,2');
+INSERT INTO test.products (serial_number, name, price) VALUES ('9417817914', 'Salicylic Acid', '7,4,5');
+INSERT INTO test.products (serial_number, name, price) VALUES ('9483634008', 'Cabbage', '7,10,8');
